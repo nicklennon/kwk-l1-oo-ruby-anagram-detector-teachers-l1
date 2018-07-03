aba083e0b1ad3b8c1e83055d3e6db1aefb7e32a8
nicklennon <nicklennon.53@gmail.com>
2018-07-03 19:10:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,11 +10,10 @@ class Anagram
     all_matches=[]
     my_letters = @word.split("").sort
     array.each do |array_word|
-       #array_of_letters = 
-       puts array_word.split("").sort
-    #   #if my_letters == array_of_letters.sort
-    #   #   all_matches << array_word
-    #   # end
+       array_of_letters = array_word.split("").sort
+       if my_letters == array_of_letters
+         all_matches << array_word
+       end
     end
     all_matches
   end
