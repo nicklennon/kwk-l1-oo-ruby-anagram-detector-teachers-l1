6236c6ec4555a0274b32bc04f62fade9046e98cb
nicklennon <nicklennon.53@gmail.com>
2018-07-03 19:07:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,15 +7,15 @@ class Anagram
   end
   
   def match(array)
-    all_matches=[]
-    my_letters = @word.split("").sort
-    # array.each do |array_word|
-    #   #array_of_letters = 
-    #   puts array_word.split("").sort
-      #if my_letters == array_of_letters.sort
-      #   all_matches << array_word
-      # end
-      return all_matches
+    # all_matches=[]
+    # my_letters = @word.split("").sort
+    # # array.each do |array_word|
+    # #   #array_of_letters = 
+    # #   puts array_word.split("").sort
+    #   #if my_letters == array_of_letters.sort
+    #   #   all_matches << array_word
+    #   # end
+    #   return all_matches
   end
   
   
